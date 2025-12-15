03962bd2b96fc80d37a3e5ca7f7d1111eb09de39
Neto test <veronikakaravanova60@gmail.com>
2025-12-15 21:09:12 +0500

    fix: исправление кодировки BOM и совместимость Java 11 + Gradle 7.6.4

--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,9 @@
 @echo off
-rem Minimal Gradle wrapper for Windows
-rem If wrapper doesn't work, use system Gradle
-
-set GRADLE_VERSION=8.5
-set GRADLE_CMD=gradle
-
-rem Check if wrapper jar exists
-if exist "gradle\wrapper\gradle-wrapper.jar" (
-    rem Use wrapper
-    java -jar gradle\wrapper\gradle-wrapper.jar %*
-) else (
-    rem Use system gradle
+set JAVA_HOME=%JAVA_HOME%
+set GRADLE_HOME=%GRADLE_HOME%
+if "%GRADLE_HOME%"=="" (
     echo Using system Gradle...
-    %GRADLE_CMD% %*
+    gradle %*
+) else (
+    "%GRADLE_HOME%\bin\gradle" %*
 )
